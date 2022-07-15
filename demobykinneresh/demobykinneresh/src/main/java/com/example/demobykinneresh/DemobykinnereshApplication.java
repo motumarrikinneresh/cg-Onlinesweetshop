200b8cf05321803ad7a8425757cf9ce38618af3b
motumarrikinneresh <84170153+motumarrikinneresh@users.noreply.github.com>
2022-07-15 11:37:11 +0530

    Add files via upload

---
+++ demobykinneresh/demobykinneresh/src/main/java/com/example/demobykinneresh/DemobykinnereshApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobykinneresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobykinnereshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobykinnereshApplication.class, args);
+	}
+
+}
---
+++ demobykinneresh/demobykinneresh/src/test/java/com/example/demobykinneresh/DemobykinnereshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobykinneresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobykinnereshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
